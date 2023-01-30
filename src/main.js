07e41981dcba2d30b5b20888ed59cc7810376c1d
weihan.wu <904698202@qq.com>
2023-01-30 21:33:12 +0800

    数据库连接，注册接口写入数据库

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const config = require('./app/config')
-
+require('./app/database')
 app.listen(config.APP_PORT, () => {
   console.log(`服务器启动成功 ===> localhost:${config.APP_PORT}`);
 })
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,11 @@
+const connection = require('../app/database')
+
 class UserService {
   async create(user) {
+    const { name, password } = user
     // 将user存入数据库
-    return JSON.stringify(user)
+    const statement = `INSERT INTO users (name,password) VALUES (?,?);`
+    return await connection.execute(statement, [name, password])
   }
 }
 
