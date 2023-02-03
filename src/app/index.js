54fe74bace23fa3c7bb0bb6331cfcc6f92738f64
weihan.wu <904698202@qq.com>
2023-02-03 22:12:05 +0800

    路由文件自动加载

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,13 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const useRoutes = require('../router')
+const errorHandler = require('./error-handle')
+
 const app = new Koa()
 
-const userRouter = require('../router/user.router')
-const authRouter = require('../router/auth.router')
-
-const errorHandler = require('./error-handle')
-const { routes } = require('../router/auth.router')
-
 app.use(bodyParser())
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
+app.useRoutes = useRoutes
+app.useRoutes()
 
 app.on('error', errorHandler)
 
